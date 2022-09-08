ed070feabeb5437034737fb3fb1ea906e776b3de
annettesharon <annettesharon@ymail.com>
2022-09-07 20:05:06 -0500

    Java ls4 exercises commit

--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -9,9 +9,28 @@ public class Course {
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+    public String toString() {
+        String courseReport = String.format("%s is a %s with %d as instructor and the enrolled students are %.2f", this.topic, this.instructor, this.enrolledStudents);
+        return courseReport;
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
 
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Course theCourse = (Course) toBeCompared;
+        return theCourse.getClass() == getClass();
+    }
 }
